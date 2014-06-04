5f2e7ec1d7a736cea42e24bd3e9516a39588549d
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-04 23:06:43 +0100

    Add experimental placement methods. Test with group tomorrow.

--- lib/grid.rb
+++ lib/grid.rb
@@ -17,9 +17,20 @@ class Grid
     @board
   end
 
-  def place(ship,position)
-    @board[position] = ship
-    board
+  # place method iterates through coordinates and creates a new array with incremented values.
+  # these values are then used to place the ship into the grid.
+  def place_vertical(ship, coordinate)
+    array = 0.upto(ship.size - 1).map do |i|
+      [(coordinate.split('')[0].ord + i).chr, coordinate.split('')[1]].join
+    end
+    array.each{|coordinate| @grid[coordinate] = :ship}
+  end
+
+  def place_horizontal(ship, coordinate)
+    array = 0.upto(ship.size - 1).map do |i|
+      [coordinate.split('')[0], (coordinate.split('')[1].ord + i).chr].join
+    end
+    array.each{|coordinate| @grid[coordinate] = :ship}
   end
 
   def check(coordinate)
@@ -31,6 +42,13 @@ class Grid
       "Miss"
     else
       check(coordinate).hit!
+    end
+  end
+
+  def rotator(input, length, ship)
+    0.upto(length - 1).map do |i|
+      var = [(input.split('')[0].ord + i).chr, input.split('')[1]].join
+    place(ship,var)
     end
   end
 
--- lib/ship.rb
+++ lib/ship.rb
@@ -24,3 +24,39 @@ class Ship
   end
 
 end
+
+class AircraftCarrier
+  def initialize
+    @size = 5
+  end
+  attr_reader :size
+end
+
+class Dreadnought
+  def initialize
+    @size = 4
+  end
+  attr_reader :size
+end
+
+class Cruiser
+  def initialize
+    @size = 3
+  end
+  attr_reader :size
+end
+
+class Destroyer
+  def initialize
+    @size = 2
+  end
+  attr_reader :size
+end
+
+class Submarine
+  def initialize
+    @size = 1
+  end
+
+  attr_reader :size
+end
