8678b300981a97da3154ac9fa326ad28634e3ff3
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-04 15:43:40 +0100

    Creates the grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,26 +1,20 @@
 class Grid
+
   def initialize
-    @board = [:sea, :sea]
+    @board = {}
+    create_grid
+  end
+
+  def create_grid
+    ('A'..'J').to_a.each do |letter|
+      (1..10).to_a.each do |number|
+        @board["#{letter}#{number}"] = "sea "
+      end
+    end
   end
 
   def board
     @board
   end
 
-  def place(ship,position)
-    @board[position] = ship
-    board
-  end
-
-  def check(coordinate)
-    @board[coordinate]
-  end
-
-  def fire_at(coordinate)
-    if check(coordinate) == :sea
-      "Miss"
-    else
-      check(coordinate).hit!
-    end
-  end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -6,13 +6,8 @@ describe 'Grid' do
 	let (:ship) {Ship.new}
 	let (:grid) {Grid.new}
 
-		it 'has width 2' do
-			expect(grid.board.count).to eq 2
-		end
-
-		it 'can check if a ship is at a coordinate' do
-			grid.place(ship,0)
-			expect(grid.check(0)).to eq ship
+		it 'is created after initialization' do
+			expect(grid.board).to_not eq {}
 		end
 
 end
